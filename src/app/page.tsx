8fe685c7e6a4855852adc42c2e6593862e090eb6
zeamanuel fetene <zeamanuel1994@gmail.com>
2024-12-01 07:30:29 +0300

    Create Home page with Hero section, feature overview, and step-by-step guide.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,81 @@ export default function Home() {
     
     {/* feature lading page #this is where we know what can the website do   */}
           
-    
+    <div className=" mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+      <div className="mb-12 px-6 lg:px-8"> 
+        <div className="mx-auto max-w-2xl sm:text-center"> 
+          <h2 className="mt-2 front-bold text-4xl text-gray-800 sm:text-5xl"> 
+            start chating in a minutes </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              chatting with this app make you pdf and other reding materials amzaing and better it will make it cool.
+              
+            </p>
+
+        </div>
+      </div>
+
+      {/*  steps*/}
+
+      <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-500 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0  md:pt-4">
+            <span className="text-sm font-medium text-blue-500 ">step 1</span>
+            <span className="text-xl font-semibold">
+              SIGN UP for an account
+            </span>
+            <span className="mt-2 text-zinc-600">
+              Either staring out with a free paln or choose our {''}
+              <Link 
+              href='/pricing'
+              className="text-blue-700 underline-offset-2">
+                  pro plan.
+              </Link>
+            </span>
+          </div>
+        </li>
+
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-500 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0  md:pt-4">
+            <span className="text-sm font-medium text-blue-500 ">step 2</span>
+            <span className="text-xl font-semibold">
+              UPLOAD your PDf file 
+              </span>
+            <span className="mt-2 text-zinc-600">
+              we&apos;ll process file and make it ready for you to chat with.
+              
+            </span>
+          </div>
+        </li>
+
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-500 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0  md:pt-4">
+            <span className="text-sm font-medium text-blue-500 ">step 3</span>
+            <span className="text-xl font-semibold">
+              START asking questions 
+              </span>
+            <span className="mt-2 text-zinc-600">
+              it&apos;s that simple . TRY out quill today - it really take a less than a minuites.
+
+
+            </span>
+          </div>
+        </li>
+      </ol>
+
+      <div className="mx-auto max-w-6xl px-6 lg:px-8 ">
+            <div className="mt-16 flow-root sm:mt-24 ">
+              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4  ">
+                <img 
+                src="/file-upload-preview.jpg"
+                alt="upload view"
+                width={1419} height={732}
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10 "
+                />
+              </div>
+            </div>
+          </div>
+    </div>
     
     
     
